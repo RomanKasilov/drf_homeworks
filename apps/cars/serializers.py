93c9311b6efcf858bb3b892670570786c6f56d02
RomanKasilov <KasiaKasilov@gmail.com>
2024-08-06 21:14:04 +0300

    test_validations added

--- apps/cars/serializers.py
+++ apps/cars/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.serializers import ValidationError
 
 from apps.cars.models import CarModel
 
@@ -6,4 +7,14 @@ from apps.cars.models import CarModel
 class CarSerializer(serializers.ModelSerializer):
     class Meta:
         model = CarModel
-        fields = ('id', 'brand', 'price', 'year')
+        fields = ('id', 'brand', 'price', 'year','body_type')
+
+    # def validate(self, data):
+    #     if data['price'] == data['year']:
+    #         raise ValidationError({'details': 'fields price and year are the same'})
+    #     return data
+    #
+    # def validate_year(self, year):
+    #     if year == 2024:
+    #         raise ValidationError({'details': 'year = 2024 is forbidden'})
+    #     return year

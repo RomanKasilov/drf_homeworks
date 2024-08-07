6677ffd2b3bcfa6fedfb8160cd1b7494b484deb5
RomanKasilov <KasiaKasilov@gmail.com>
2024-08-07 19:47:05 +0300

    migrations fixed

--- apps/cars/models.py
+++ apps/cars/models.py
@@ -11,6 +11,7 @@ from apps.cars.choices.body_type_choice import BodyTypeChoice
 class CarModel(BaseModel):
     class Meta:
         db_table = 'cars'
+        ordering = ('id',)
 
     brand = models.CharField(max_length=255, validators=(V.MinLengthValidator(2),))
     price = models.IntegerField(validators=(V.MinValueValidator(0), V.MaxValueValidator(1_000_000)))

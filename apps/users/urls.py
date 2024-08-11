5216b2f586c9de4270570529e5d86336cf4f03d1
RomanKasilov <KasiaKasilov@gmail.com>
2024-08-11 16:07:23 +0300

    homework done

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 
-from apps.users.views import UserListCreateView
+from apps.users.views import UserAdminDelView, UserAdminPassView, UserBlockView, UserListCreateView, UserUnBlockView
 
 urlpatterns = [
-    path('', UserListCreateView.as_view())
+    path('', UserListCreateView.as_view()),
+    path('/<int:pk>/block', UserBlockView.as_view()),
+    path('/<int:pk>/unblock', UserUnBlockView.as_view()),
+    path('/<int:pk>/admin_pass', UserAdminPassView.as_view()),
+    path('/<int:pk>/admin_del', UserAdminDelView.as_view()),
+
 ]
